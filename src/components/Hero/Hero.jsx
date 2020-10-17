43bccb87c65e6b8f7cc4b1dd9ecdb2c49489b566
SoyChechoGonzalez <SoySergioGonzalez@Outlook.es>
2020-10-17 02:54:04 -0500

    Se añade typewriter y se opaca un poco la imagen del hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,17 +8,26 @@ const Hero = () => {
       <section className="hero">
         <article className="hero__content">
           <h1>Diseño & Espacio</h1>
+          <h2 className="typewriter" dataText="By. Luisa Betancur">
+            By. Luisa Betancur
+          </h2>
         </article>
         <article className="social">
           <ul>
             <li>
-              <a href="/" className="FaInstagram">
-                <FaInstagram size="4rem" />
+              <a
+                href="https://www.instagram.com/disenoespacios.luisab/"
+                className="FaInstagram"
+              >
+                <FaInstagram size="3rem" />
               </a>
             </li>
             <li>
-              <a href="/" className="FaFacebook">
-                <FaFacebook size="4rem" />
+              <a
+                href="https://www.instagram.com/disenoespacios.luisab/"
+                className="FaFacebook"
+              >
+                <FaFacebook size="3rem" />
               </a>
             </li>
           </ul>
