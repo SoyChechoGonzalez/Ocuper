ad2484bfec22e70b5fc2e7f56340e72db1f42a26
SoyChechoGonzalez <SoySergioGonzalez@Outlook.es>
2020-10-16 23:16:25 -0500

    Se agregan los iconos de Redes Sociales

---
+++ src/assets/images.jsx
@@ -0,0 +1,4 @@
+import fachada from "../assets/img/fachada.jpeg";
+import otro from "../assets/img/otro.jpg";
+
+export default [fachada, otro];
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Hero.css";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 const Hero = () => {
   return (
     <React.Fragment>
-      <div className="hero">
-        <h1>Helloo</h1>
-      </div>
+      <section className="hero">
+        <article className="hero__content">
+          <h1>Diseño & Espacio</h1>
+        </article>
+        <article className="social">
+          <ul>
+            <li>
+              <a href="/" className="FaInstagram">
+                <FaInstagram size="4rem" />
+              </a>
+            </li>
+            <li>
+              <a href="/" className="FaFacebook">
+                <FaFacebook size="4rem" />
+              </a>
+            </li>
+          </ul>
+        </article>
+      </section>
     </React.Fragment>
   );
 };
