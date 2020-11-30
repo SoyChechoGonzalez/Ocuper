a852f3c8d9f5861a9497f1fbad40d35cc07eabe0
SoyChechoGonzalez <SoySergioGonzalez@Outlook.es>
2020-11-29 20:47:59 -0500

    Se agregan todas la secciones de imágenes y descripción de cada contenido

--- src/components/Interiorismo/Interiorismo.jsx
+++ src/components/Interiorismo/Interiorismo.jsx
@@ -30,11 +30,48 @@ const Interiorismo = () => {
               <div className="sub-item imagen"></div>
               <div className="sub-item imagen"></div>
             </div>
-            <div className="item">Item2</div>
-            <div className="item">Item3</div>
-            <div className="item">Item4</div>
-            <div className="item">Item5</div>
-            <div className="item">Item6</div>
+            <div className="item">
+              <h1>Casa 3/13 Dos pisos</h1>
+              <p>
+                Proyecto de diseño interior que buscaba ampliar el espacio
+                original, con dos pisos, el primero zonas comunes y el segundo
+                habitaciones y baños buscando espacios amplios y abiertos,
+                cuenta con dos habitaciones entre ellas la principal con baño y
+                Vestier.
+              </p>
+            </div>
+            <div className="item">
+              <h1>Apartamentos Gaia- Área1</h1>
+              <p>
+                Plantas ambientadas para la empresa Área 1 y su proyecto Gaia
+                ubicado en La Ceja- Antioquia.
+              </p>
+            </div>
+            <div className="item">
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+            </div>
+            <div className="item">
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+              <div className="sub-item imagen"></div>
+            </div>
+            <div className="item">
+              <h1>Zona húmeda la purísima- Área 1</h1>
+              <p>
+                Un proyecto de la empresa Área 1 en la que se requería un
+                espacio exterior interactivo a necesidad de sus clientes, que
+                contara con zona húmeda y un sector para el descanso y la
+                diversión.
+              </p>
+            </div>
           </div>
         </article>
       </section>
